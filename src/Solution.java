47368c5cb182549d69b8328e71829e01935b89ca
Vladimir Belkin <vladimirB2000@mail.ru>
2019-08-06 10:12:41 +0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <=10){
+            int b = 1;
+            while (b <= 10)
+            {System.out.print (a*b + "  ");
+                b++; }
+            System.out.println();
+            a++; } //напишите тут ваш код
+
+    }
+}
+
+
